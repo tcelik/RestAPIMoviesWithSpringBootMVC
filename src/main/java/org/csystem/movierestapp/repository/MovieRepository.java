80a239d00dd57b73dc566cfde3bdece0724d74d8
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2019-01-18 15:08:48 +0300

    011-AnlaşmayıBoşunaYazmadık

--- src/main/java/org/csystem/movierestapp/repository/MovieRepository.java
+++ src/main/java/org/csystem/movierestapp/repository/MovieRepository.java
@@ -1,12 +1,61 @@
 package org.csystem.movierestapp.repository;
 
+import org.csystem.movierestapp.entity.MovieInfo;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 //Dependency injection için anatosyonlar
 @Repository
 @Transactional
-public class MovieRepository {
+public class MovieRepository implements IMovieRepository{
+    @Override
+    public Iterable<MovieInfo> findByName(String name)
+    {
+        return null;
+    }
+
+    @Override
+    public Iterable<MovieInfo> findByYear(int year)
+    {
+        return null;
+    }
+
+    @Override
+    public Iterable<MovieInfo> findByYears(int start, int end)
+    {
+        return null;
+    }
+
+    @Override
+    public Iterable<MovieInfo> findByDirector(String director)
+    {
+        return null;
+    }
+
+    @Override
+    public Iterable<MovieInfo> findAll()
+    {
+        return null;
+    }
+
+    @Override
+    public Optional<MovieInfo> findById()
+    {
+        return Optional.empty();
+    }
+
+    @Override
+    public <E extends MovieInfo> E save(E e)
+    {
+        return null;
+    }
+
+    @Override
+    public void delete(MovieInfo e)
+    {
+
+    }
 
 }
