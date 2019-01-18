a572dad66136f93747784077ed6c76b3afb779d8
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2019-01-18 15:06:57 +0300

    010-MovieRepositoryMovieDao

--- src/main/java/org/csystem/movierestapp/repository/MovieRepository.java
+++ src/main/java/org/csystem/movierestapp/repository/MovieRepository.java
@@ -1,5 +1,12 @@
 package org.csystem.movierestapp.repository;
 
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+//Dependency injection için anatosyonlar
+@Repository
+@Transactional
 public class MovieRepository {
 
 }
