2920e801c190685f90433a4ea2c94ebab089623d
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2019-01-18 15:26:42 +0300

    015-IMovieServiceRepoServiceAslında

--- src/main/java/org/csystem/movierestapp/service/IMovieService.java
+++ src/main/java/org/csystem/movierestapp/service/IMovieService.java
@@ -4,5 +4,7 @@ import org.csystem.movierestapp.entity.MovieInfo;
 import org.csystem.service.IService;
 
 public interface IMovieService extends IService<MovieInfo, Long> {
-
+    Iterable<MovieInfo> findByName(String name); //repo ne ise o aslında. Servis repoyu servis ediyor.s
+    Iterable<MovieInfo> findByYear(int year);
+    Iterable<MovieInfo> findByYears(int start, int end);
 }
