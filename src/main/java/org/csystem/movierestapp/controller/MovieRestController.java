261b63a8945bff98a0cb0926e16f28330072ce8e
Tuğberk Çelik <mehmettugberkcelik@gmail.com>
2019-01-18 16:14:48 +0300

    018-MovieRestController

---
+++ src/main/java/org/csystem/movierestapp/controller/MovieRestController.java
@@ -0,0 +1,10 @@
+package org.csystem.movierestapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movies") //bu url altından başlasın mapping işlemler, /movies controller
+public class MovieRestController {
+
+}
